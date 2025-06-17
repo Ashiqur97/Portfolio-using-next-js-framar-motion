44f20242b73b089d4f3f0424435dcf800a4a050b
Ashiqur97 <ashiqurrahmanabir96@gmail.com>
2025-06-17 20:38:18 +0600

    fixing some error

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,10 +1,12 @@
 "use client"
 
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import React,{useState} from 'react'
 
 const Navbar = () => {
     const [isMobileMenuOpen,setIsMobileMenuOpen] = useState<boolean>(false);
+    const pathname = usePathname();
     const toggleMobileMenu = () => {
         setIsMobileMenuOpen(!isMobileMenuOpen);
     }
@@ -17,10 +19,23 @@ const Navbar = () => {
     { href: '/contact', label: 'Contact' },
   ];
   return (
-    <nav className='fixed w-full bg-white/80 dark:bg-dark/80 backdrop-blur-sm z-50'>
+    <nav className='fixed w-full  dark:bg-dark/80 backdrop-blur-sm z-50'>
         <div className='container max-w-7xl mx-auto px-4'>
-            <div>
-                <Link href="/">Ashiqur Rahman</Link>
+            <div  className="flex items-center justify-between h-16">
+                <Link  href="/" className="text-xl font-bold text-primary">Ashiqur Rahman</Link>
+
+                <div className='hidden md:flex items-center space-x-8'>
+                           {menuItems.map((item) => (
+                            const isActive = pathname === item.href;
+              <Link 
+                key={item.href}
+                href={item.href} 
+                className={`hover:text-primary transition-colors ${isActive ? 'font-semibold text-primary' : ''}`}
+              >
+                {item.label}
+              </Link>
+            ))}
+                </div>
             </div>
         </div>
     </nav>
