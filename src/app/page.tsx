527826780c33d5670510977a1206554069bbef5c
Ashiqur97 <ashiqurrahmanabir96@gmail.com>
2025-06-20 17:24:14 +0600

    changing and updating

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from './components/Hero'
+import Projects from './components/Projects'
 
 const page = () => {
   return (
     <main>
       <Hero/>
+      <Projects />
     </main>
   )
 }
