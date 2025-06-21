63bf32c6503b1ef3f7fe333f934506fc68f36fd8
Ashiqur97 <ashiqurrahmanabir96@gmail.com>
2025-06-21 17:01:22 +0600

    change and update

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,6 +1,15 @@
 const ContactPage = () => {
     return (
-        <div>Contact Page</div>
+        <div className="container max-w-7xl mx-auto py-20">
+            <h1 className="text-4xl font-bold mb-8 text-center">Contact Me</h1>
+
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-12">
+                <div className="space-y-8">
+                    <h2 className="text-2xl font-semibold mb-4">Get in Touch</h2>
+                    <p className="text-secondary md:w-2/3">I'm always open to discussing new projects, creative ideas or opportunties to be part of your visions</p>
+                </div>
+            </div>
+        </div>
     )
 }
 
