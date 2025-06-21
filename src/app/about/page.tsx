98da3735f9196ddd9e9d4ec6d8df7935899713c4
Ashiqur97 <ashiqurrahmanabir96@gmail.com>
2025-06-21 14:49:00 +0600

    code change and update

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { FaCode } from 'react-icons/fa';
+import { FaCode, FaGraduationCap, FaLaptopCode } from 'react-icons/fa';
 
 const About = () => {
   return (
@@ -19,6 +19,60 @@ const About = () => {
           <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
             <div className='bg-white dark:bg-dark/50 p-6 rounded-lg shadow-md'>
               <FaCode className="h-8 w-8 text-primary mb-4" />
+              <h3 className='text-xl font-semibold mb-2'>Frontend</h3>
+              <ul className='text-seconadary space-y-2'>
+                <li>React/Next.js</li>
+                <li>Typescript</li>
+                <li>Tailwindcss</li>
+                <li>HTML5/CSS</li>
+              </ul>
+            </div>
+
+             <div className='bg-white dark:bg-dark/50 p-6 rounded-lg shadow-md'>
+              <FaLaptopCode className="h-8 w-8 text-primary mb-4" />
+              <h3 className='text-xl font-semibold mb-2'>Backend</h3>
+              <ul className='text-seconadary space-y-2'>
+                <li>Solidity</li>
+                <li>Hardhat</li>
+                <li>Etherjs</li>
+              </ul>
+            </div>
+
+              <div className='bg-white dark:bg-dark/50 p-6 rounded-lg shadow-md'>
+              <FaGraduationCap className="h-8 w-8 text-primary mb-4" />
+              <h3 className='text-xl font-semibold mb-2'>Backend</h3>
+              <ul className='text-seconadary space-y-2'>
+                <li>Git/GitHub</li>
+                <li>CI/CD</li>
+              </ul>
+            </div>
+          </div>
+      </section>
+
+      <section className='mb-16'>
+          <h2 className='section-title'>Expriences</h2>
+
+          <div className='max-w-3xl mx-auto space-y-8'>
+            <div className='bg-white dark:bg-dark/50 p-6 rounded-lg shadow-md'>
+                <h3 className='text-xl font-semibold mb-2'>Junior Developer</h3>
+                <p className='text-primary mb-2'>Company Name 2024</p>
+                <ul className='text-secondary space-y-2 list-disc list-inside'>
+                  <li>Multiple Web Application using asp.net core/MVC and SQL Server</li>
+                </ul>
+            </div>
+          </div>
+      </section>
+
+      <section className='mb-16'>
+          <h2 className='section-title'>Academic Qualification</h2>
+
+          <div className='max-w-3xl mx-auto space-y-8'>
+            <div className='bg-white dark:bg-dark/50 p-6 rounded-lg shadow-md'>
+                <h3 className='text-xl font-semibold mb-2'>B.Sc in Software Engineering</h3>
+                <p className='text-primary mb-2'>Daffodil International University 2019-2013</p>
+                <p className="text-secondary">
+              Graduated with honors. Focused on software engineering and web development.
+            </p>
             </div>
           </div>
       </section>
--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -11,7 +11,7 @@ export default function Footer() {
               Ashiqur Rahman
             </Link>
             <p className="text-sm text-secondary mt-2">
-              © {new Date().getFullYear()} Devfolio. All rights reserved.
+              © {new Date().getFullYear()} All rights reserved.
             </p>
           </div>
           
